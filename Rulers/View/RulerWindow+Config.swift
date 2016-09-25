faa900035532189e7d83a1202834eb0b6a87171b
Arthur Hammer <arthur@ahammer.me>
2016-09-25 09:23:04 +0200

    Add basic positioning method

--- Rulers/View/RulerWindow+Config.swift
+++ Rulers/View/RulerWindow+Config.swift
@@ -3,11 +3,44 @@ import Cocoa
 extension RulerWindow {
 
     func apply(config: WindowConfig) {
-        collectionBehavior = config.joinsAllSpaces ? .canJoinAllSpaces : []
-        alphaValue = config.alpha.cg
+        applyStyle(for: config)
+        applyPosition(for: config)
+    }
 
+    func applyStyle(for config: WindowConfig) {
+        collectionBehavior = config.joinsAllSpaces ? .canJoinAllSpaces : []
+        hasShadow = config.hasShadow
+        alphaValue = config.alpha.cg
         backgroundColor = .clear
         rulerView.backgroundColor = config.color.withAlphaComponent(1)
         rulerView.cornerRadius = config.cornerRadius
+    }
+
+    func applyPosition(for config: WindowConfig) {
+        guard let screen = screen else {  // FIXME: Extract screen/mouse handling?
+            Swift.print("No screen found.")
+            return
+        }
+
+        let newFrame = frame(for: config,
+                             screenFrame: screen.frame,
+                             mouseLocation: screen.frame.center)  // FIXME
+        setFrame(newFrame, display: true)
+    }
+
+    func frame(for config: WindowConfig,
+               screenFrame: CGRect,
+               mouseLocation: CGPoint) -> CGRect {
+
+        let size = config.size.resolve(with: screenFrame.size)
+        let center = mouseLocation
+        let offset = config.mouseOffset
+        let clamping = config.canMoveOffscreen ? .infinite : screenFrame
+
+        return CGRect(origin: .zero, size: size)
+            .centered(at: center)
+            .offset(by: offset)
+            .clamped(to: clamping)
+            .integral
     }
 }
