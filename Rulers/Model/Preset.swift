d72093978dd122b8eaa55f8dd2042e3827a11977
Arthur Hammer <arthur@ahammer.me>
2016-09-29 23:41:42 +0200

    Add preset id

--- Rulers/Model/Preset.swift
+++ Rulers/Model/Preset.swift
@@ -1,13 +1,27 @@
 import Foundation
 
 struct Preset {
+    var id: String
     var name = NSLocalizedString("New Preset", comment: "Name of new preset")
     var config = WindowConfig()
+
+
+    init(id: String = NSUUID().uuidString,  // FIXME: Should this be assigned here?
+         name: String = NSLocalizedString("New Preset", comment: "Name of new preset"),
+         config: WindowConfig = WindowConfig()) {
+
+        self.id = id
+        self.name = name
+        self.config = config
+    }
 }
 
 
 extension Preset: Equatable {
-    static func == (l: Preset, r: Preset) -> Bool {
-        return l.name == r.name && l.config == r.config
+    static func == (lhs: Preset, rhs: Preset) -> Bool {
+        return
+            lhs.id == rhs.id
+            lhs.name == rhs.name &&
+            lhs.config == rhs.config
     }
 }
